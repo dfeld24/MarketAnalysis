7c08e3825bce77d4062c3b5f20a2272d727f53e4
dfeld24 <darcyfeld@limaconleche.com>
2025-08-12 09:12:48 -0700

    Update to print items

--- script.py
+++ script.py
@@ -9,8 +9,39 @@ from bs4 import BeautifulSoup
 url = "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
 page = requests.get(url)
 
-#See html source
-print(page.content)
-
 # Turn HTML to BeautifulSoup object
 soup = BeautifulSoup(page.content, 'html.parser')
+
+# Product Page URL
+product_page_url = url
+print(url)
+
+# UPC
+upc_element = soup.select_one("#content_inner > article > table > tr:nth-child(1) > td")
+upc = upc_element.text
+print(upc)
+
+# Price (excl. tax)
+price_excludes_tax = soup.select_one("#content_inner > article > table > tr:nth-child(3) > td")
+price_excludes_tax = price_excludes_tax.text
+print(price_excludes_tax)
+
+# Price (inc. tax)
+price_includes_tax = soup.select_one("#content_inner > article > table > tr:nth-child(3) > td")
+price_includes_tax = price_includes_tax.text
+print(price_includes_tax)
+
+# Quantity Available
+quantity_available = soup.select_one("#content_inner > article > table > tr:nth-child(6) > td")
+quantity_available = quantity_available.text
+print(quantity_available)
+
+# Product Description
+product_description = soup.select_one("#content_inner > article > p")
+product_description = product_description.text
+print(product_description)
+
+# Category
+category = soup.select_one("#default > div > div > ul > li:nth-child(3) > a")
+category = category.text
+print(category)
